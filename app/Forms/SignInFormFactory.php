fa1d641b1136706998decce5b63accc9337fe67e
adamsvenk <svenk.adam@gmail.com>
2020-11-30 17:03:41 +0100

    FE update

--- app/Forms/SignInFormFactory.php
+++ app/Forms/SignInFormFactory.php
@@ -26,15 +26,15 @@ final class SignInFormFactory
 	public function create(callable $onSuccess): Form
 	{
 		$form = new Form();
-		$form->addText('username', 'Username:')
-			->setRequired('Please enter your username.');
+		$form->addText('username', 'Uživatelské jméno:')
+			->setRequired('Zadejte své uživatelské jméno.');
 
-		$form->addPassword('password', 'Password:')
-			->setRequired('Please enter your password.');
+		$form->addPassword('password', 'Heslo:')
+			->setRequired('Prosím zadejte své heslo.');
 
-		$form->addCheckbox('remember', 'Keep me signed in');
+		$form->addCheckbox('remember', 'Zůstat přihlášen');
 
-		$form->addSubmit('send', 'Sign in');
+		$form->addSubmit('send', 'Přihlásit se');
 
 		$form->onSuccess[] = function (Form $form, \stdClass $values) use ($onSuccess): void {
 			try {
